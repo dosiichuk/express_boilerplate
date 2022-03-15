cf9dd652a8a36dadfecc76ba8bf57bbd90b0af71
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-15 21:28:41 +0100

    Add express boilerplate

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.show = (name) => {
+    res.sendFile(path.join(__dirname, `/views/${name}`));
+  };
+  next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/user', (req, res, next) => {
+  res.show('login.html');
+  //   next();
+});
+
+app.get(['/', '/home'], (req, res) => {
+  res.show('index.html');
+});
+
+app.get('/about', (req, res) => {
+  res.show('about.html');
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
