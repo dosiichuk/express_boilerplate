9a85ec4e6e6ba825626b96b890cf818d3d386ee5
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-23 15:12:30 +0100

    Add multer and image upload

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, path.join(__dirname, '/public'));
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+const upload = multer({ storage });
+
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/user', (req, res, next) => {
   res.render('login');
@@ -24,6 +36,21 @@ app.get('/hello/:name', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.post('/contact/send-message', upload.single('design'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  console.log(author, sender, title, message, req.file.originalname);
+  if (author && sender && title && message && req.file) {
+    res.render('contact', { isSent: true, fileName: req.file.originalname });
+  } else {
+    res.render('contact', { isError: true });
+  }
 });
 
 app.use((req, res) => {
