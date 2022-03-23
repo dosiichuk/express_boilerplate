9c97054962227e722a704697863e4bb02110886e
Dmytro <dmytro.osiychuk@gmail.com>
2022-03-23 13:26:45 +0100

    Add templating engine

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/user', (req, res, next) => {
-  res.show('login.html');
+  res.render('login', { layout: false });
   //   next();
 });
 
 app.get(['/', '/home'], (req, res) => {
-  res.show('index.html');
+  res.render('index', { layout: false });
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
 });
 
 app.use((req, res) => {
-  res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  res.status(404).render('404.hbs', { layout: false });
 });
 
 app.listen(8000, () => {
